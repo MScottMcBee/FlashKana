f9461ede865fe53d6e360cdd8a8c5ada1a1a4dc5
ScottMcBee <scott@dragonarmy.com>
2018-06-30 18:53:07 -0400

    starting on refactor

--- FlashKanaAndroid/app/src/main/java/com/mscottmcbee/flashkana/ui/kanaview/KanaViewFragment.kt
+++ FlashKanaAndroid/app/src/main/java/com/mscottmcbee/flashkana/ui/kanaview/KanaViewFragment.kt
@@ -6,12 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.mscottmcbee.flashkana.R
+import com.mscottmcbee.flashkana.R.id.fragment_kana_view_answer
+import com.mscottmcbee.flashkana.R.id.fragment_kana_view_glyph
 import com.mscottmcbee.flashkana.model.KanaObject
 import kotlinx.android.synthetic.main.fragment_kana_view.*
+import android.databinding.DataBindingUtil
+import android.databinding.ViewDataBinding
+import com.mscottmcbee.flashkana.databinding.FragmentKanaViewHelloBinding
+
 
 class KanaViewFragment : Fragment(), KanaViewContract.View {
 
     override lateinit var presenter: KanaViewContract.Presenter
+
+    lateinit var viewModel: KanaViewViewModel
 
     companion object {
         fun newInstance(): KanaViewFragment {
@@ -19,28 +27,33 @@ class KanaViewFragment : Fragment(), KanaViewContract.View {
         }
     }
 
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.fragment_kana_view, container, false)!!
+        val viewModel = KanaViewViewModel()
+        val binding = FragmentKanaViewHelloBinding.inflate(inflater,container,false)
+        binding.viewmodel = viewModel
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        fragment_kana_view_button.setOnClickListener { _ ->
-            presenter.onMainClicked()
-        }
+    //    fragment_kana_view_button.setOnClickListener { _ ->
+    //        presenter.onMainClicked()
+     //   }
 
-        presenter.setup()
+      //  presenter.setup()
     }
 
     override
     fun showKana(kanaObject: KanaObject) {
-        fragment_kana_view_glyph.text = kanaObject.glyph
-        fragment_kana_view_answer.text = kanaObject.answer
+    //    fragment_kana_view_glyph.text = kanaObject.glyph
+    //    fragment_kana_view_answer.text = kanaObject.answer
     }
 
     override fun onPause() {
         super.onPause()
-        presenter.updateCardsViewed()
+     //   presenter.updateCardsViewed()
     }
 
 }
---
+++ FlashKanaAndroid/app/src/main/java/com/mscottmcbee/flashkana/ui/kanaview/KanaViewViewModel.kt
@@ -0,0 +1,10 @@
+package com.mscottmcbee.flashkana.ui.kanaview
+
+import android.databinding.ObservableField
+import android.databinding.ObservableList
+
+class KanaViewViewModel(){
+
+    val hello = ObservableField<String>("hey")
+
+}
