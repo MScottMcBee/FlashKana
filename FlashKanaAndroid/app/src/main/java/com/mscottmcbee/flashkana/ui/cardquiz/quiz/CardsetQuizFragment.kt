8d6276f4e31d35548a8d5345c6011544a68ab9ae
Scott McBee <scott@mcb.ee>
2018-09-04 09:26:17 -0400

    better feedback for quiz

--- FlashKanaAndroid/app/src/main/java/com/mscottmcbee/flashkana/ui/cardquiz/quiz/CardsetQuizFragment.kt
+++ FlashKanaAndroid/app/src/main/java/com/mscottmcbee/flashkana/ui/cardquiz/quiz/CardsetQuizFragment.kt
@@ -1,13 +1,16 @@
 package com.mscottmcbee.flashkana.ui.cardquiz.quiz
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import com.mscottmcbee.flashkana.R
 import com.mscottmcbee.flashkana.databinding.FragmentCardsetQuizBinding
 import kotlinx.android.synthetic.main.fragment_cardset_quiz.*
@@ -43,11 +46,29 @@ class CardsetQuizFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         editText.setOnEditorActionListener { _, i, _ ->
-            when(i){
+            when (i) {
                 EditorInfo.IME_ACTION_GO -> viewModel.onClick()
             }
             true
         }
+
+        viewModel.results.observe(this, Observer { string ->
+            if (string != null && context != null) {
+                val thisContext: Context = context as Context //todo: Clean up using contracts when kotlin 1.3 is out
+                results.text = string
+                results.setTextColor(ContextCompat.getColor(thisContext, when (string) {
+                    "correct" -> R.color.correct_answer
+                    else -> R.color.wrong_answer
+                }))
+                results.alpha = 1f
+                results.animate()
+                        .alpha(0f)
+                        .setDuration(500)
+                        .setStartDelay(2000)
+                        .start()
+            }
+        })
+
         super.onViewCreated(view, savedInstanceState)
     }
 
--- FlashKanaAndroid/app/src/main/java/com/mscottmcbee/flashkana/ui/cardquiz/quiz/CardsetQuizViewModel.kt
+++ FlashKanaAndroid/app/src/main/java/com/mscottmcbee/flashkana/ui/cardquiz/quiz/CardsetQuizViewModel.kt
@@ -5,6 +5,22 @@ import androidx.lifecycle.ViewModel
 import com.mscottmcbee.flashkana.model.Repository
 import com.mscottmcbee.flashkana.model.dataobjects.Card
 import com.mscottmcbee.flashkana.model.dataobjects.CardSet
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.internal.operators.observable.ObservableReplay.observeOn
+import java.util.concurrent.TimeUnit
+import androidx.core.view.ViewCompat.setAlpha
+import androidx.coordinatorlayout.widget.CoordinatorLayout.Behavior.setTag
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
+import android.view.View
+import androidx.databinding.BindingAdapter
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.mscottmcbee.flashkana.R
+
 
 class CardsetQuizViewModel(cardSetId: String, repository: Repository) : ViewModel() {
 
@@ -14,8 +30,18 @@ class CardsetQuizViewModel(cardSetId: String, repository: Repository) : ViewMode
     private lateinit var currentCard: Card
     var answerAttempt: ObservableField<String> = ObservableField("")
 
-    var results: ObservableField<String> = ObservableField("")
+    var results: MutableLiveData<String> = MutableLiveData<String>().also { it.value="" }
+    var resultsAlpha: ObservableField<Float> = ObservableField(0f)
     var spree: ObservableField<Int> = ObservableField(0)
+
+    private var misses: Int = 0
+
+    private var disposable: CompositeDisposable = CompositeDisposable()
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.dispose()
+    }
 
     init {
         showNewCard()
@@ -25,20 +51,35 @@ class CardsetQuizViewModel(cardSetId: String, repository: Repository) : ViewMode
         var x = currentCard.answers.filter { it.toUpperCase() == answerAttempt.get()?.toUpperCase() }
         if (x.count() > 0) {
             showNewCard()
-            results.set("correct")
+            feedback("correct")
             spree.set(spree.get()?.plus(1)) // I hate it
-        }else{
-            results.set("incorrect")
+            misses = 0
+        } else {
+            misses++
+            feedback(when{
+                misses>=3 -> "answer: ${currentCard.answers[0]}"
+                else -> "incorrect"
+            })
             answerAttempt.set("")
             spree.set(0)
         }
+    }
+
+    private fun feedback(message: String) {
+        results.postValue(message)
+        disposable.dispose()
+        disposable.add(Observable.timer(2500, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe{
+                    results.postValue("")
+                })
     }
 
     fun getCardSetTitle(): String {
         return cardSet.title
     }
 
-    fun onTextChanged( s: CharSequence, start: Int, before: Int, count:Int){
+    fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
         answerAttempt.set(s.toString())
     }
 
@@ -52,5 +93,4 @@ class CardsetQuizViewModel(cardSetId: String, repository: Repository) : ViewMode
         glyph.set(currentCard.question)
         answerAttempt.set("")
     }
-
 }
